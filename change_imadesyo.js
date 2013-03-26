9aeb77cbfc29d20e5a8ac248ca56edc1180dbecb
49hacker <paranshian@gmail.com>
2013-03-27 01:15:05 +0900

    いまでしょ画像に変更するやつとりあえず

--- change_imadesyo.js
+++ change_imadesyo.js
@@ -1,1 +1,8 @@
 console.log("Kiyoshi Nishihara");
+$(function(){
+
+	setInterval(function(){
+		$("img").attr("src","http://entabe.jp/upload/images/ima_2.jpg.pagespeed.ce.u2RdDXurgg.jpg");
+	},1000);
+		
+});
