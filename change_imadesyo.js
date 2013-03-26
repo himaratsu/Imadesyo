c037890ce7aa21fb5b35a403e976dce70067cabd
49hacker <paranshian@gmail.com>
2013-03-27 02:17:38 +0900

    画像を順番にしました

--- change_imadesyo.js
+++ change_imadesyo.js
@@ -1,9 +1,12 @@
 console.log("Kiyoshi Nishihara");
+var imagePaths = new Array("images/1.jpeg","images/2.jpg","images/3.jpg","images/4.png","images/5.jpg","images/6.jpg","images/7.jpg");
 $(function(){
 
 	setInterval(function(){
-		var url = chrome.extension.getURL('images/560x373.jpeg');
-		$("img").attr("src",url);
+		$("img").each(function(i){
+			var url = chrome.extension.getURL(imagePaths[i%7]);
+			$(this).attr("src",url);
+		});
 	},1000);
 		
 });
