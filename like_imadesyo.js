16d03294188da12c2f12ec5492422f1f3928fbf9
himara2 <himaratsu@gmail.com>
2013-03-28 23:00:21 +0900

    いつコメントするのか？

--- like_imadesyo.js
+++ like_imadesyo.js
@@ -1,10 +1,11 @@
-console.log("Ryosuke Hiramatsu");
-
-$(function(){
-	setInterval(function(){
-    	var txt = $('div#contentArea').html();
-    	$('div#contentArea').html(
-    	   txt.replace(/いいね/g,'今でしょ')
-    	);
-    }, 10000);
-});
+var timer = 0;
+// document.addEventListener('DOMNodeInserted', function() {
+  if(timer) return; // timerが初期化されていなければはじく
+  // timer = setTimeout(function() {
+  	var txt = $('div#globalContainer').html();
+  	$('div#globalContainer').html(
+  		txt.replace(/コメントする/g, "いつコメントするのか？").replace(/いいね/g, "今でしょ")
+  	);
+    timer = 0; // 初期化
+  // }, 10000);
+// }, false);
