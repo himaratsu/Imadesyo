25b9fc7dd675050a1dfeeaa2e40a8a5f04da59d6
himara2 <himaratsu@gmail.com>
2013-03-31 13:32:44 +0900

    いいね->今でしょ

--- like_imadesyo.js
+++ like_imadesyo.js
@@ -1,11 +1,10 @@
-var timer = 0;
-// document.addEventListener('DOMNodeInserted', function() {
-  if(timer) return; // timerが初期化されていなければはじく
-  // timer = setTimeout(function() {
-  	var txt = $('div#globalContainer').html();
-  	$('div#globalContainer').html(
-  		txt.replace(/コメントする/g, "いつコメントするのか？").replace(/いいね/g, "今でしょ")
-  	);
-    timer = 0; // 初期化
-  // }, 10000);
-// }, false);
+$(function(){
+	setInterval(function(){
+		$("a.UFILikeLink").each(function(i){
+			var txt = $(this).html();
+			$(this).html(
+				txt.replace(/いいね/g, "今でしょ")
+			);
+		});
+	},5000);
+});
