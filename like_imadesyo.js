050b89adfcc23043651cd9591eca7c3a61453f6e
himara2 <himaratsu@gmail.com>
2013-03-31 13:43:16 +0900

    コメント->いつコメントするのか

--- like_imadesyo.js
+++ like_imadesyo.js
@@ -6,5 +6,16 @@ $(function(){
 				txt.replace(/いいね/g, "今でしょ")
 			);
 		});
+
+		$("label.uiLinkButton input").each(function(){
+			$(this).attr("value", "いつコメントするのか？")
+		});
+
+		$("div.UFIImageBlockContent span span").each(function(){
+			var txt2 = $(this).html();
+			$(this).html(
+				txt2.replace(/いいね/g, "今でしょ")
+			);
+		});
 	},5000);
 });
