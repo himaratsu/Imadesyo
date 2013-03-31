17b70cbe3cf8059e09a8785c9caa164d8dcbf725
himara2 <himaratsu@gmail.com>
2013-03-31 15:07:35 +0900

    微修正

--- like_imadesyo.js
+++ like_imadesyo.js
@@ -1,57 +1,81 @@
 $(function(){
 	setInterval(function(){
 		$("a.UFILikeLink").each(function(i){
-			var txt = $(this).html();
-			$(this).html(
-				txt.replace(/いいね/g, "今でしょ")
-			);
+			if($(this).attr("data") != "imadesyo"){
+				var txt = $(this).html();
+				$(this).html(
+					txt.replace(/いいね/g, "今でしょ")
+				);
+				$(this).attr("data", "imadesyo");
+			}
 		});
 
 		$("label.uiLinkButton input").each(function(){
-			$(this).attr("value", "いつコメントするのか？")
+			if($(this).attr("data") != "imadesyo"){
+				$(this).attr("value", "いつコメントするのか？")
+				$(this).attr("data", "imadesyo");
+			}
 		});
 
 		$("div.UFIImageBlockContent span span").each(function(){
-			var txt2 = $(this).html();
-			$(this).html(
-				txt2.replace(/いいね/g, "今でしょ")
-			);
+			if($(this).attr("data") != "imadesyo"){
+				var txt2 = $(this).html();
+				$(this).html(
+					txt2.replace(/いいね/g, "今でしょ")
+				);
+				$(this).attr("data", "imadesyo");
+			}
 		});
 
 		$("div.innerWrap textarea").each(function(){
-			$(this).attr("placeholder", "いつコメントするのか？...")
-			$(this).attr("title", "いつコメントするのか？...")
-			$(this).attr("content", "いつコメントするのか？...")
-			$(this).html("いつコメントするのか？...");
+			if($(this).attr("data") != "imadesyo"){
+				$(this).attr("placeholder", "いつコメントするのか？...")
+				$(this).attr("title", "いつコメントするのか？...")
+				$(this).attr("content", "いつコメントするのか？...")
+				$(this).html("いつコメントするのか？...");
+				$(this).attr("data", "imadesyo");
+			}
 		});
 
 		$("h5.uiStreamMessage").each(function(){
-			var txt3 = $(this).html();
-			$(this).html(
-				txt3.replace(/いいね/g, "今でしょ")
-			);
+			if($(this).attr("data") != "imadesyo"){
+				var txt3 = $(this).html();
+				$(this).html(
+					txt3.replace(/いいね/g, "今でしょ")
+				);
+				$(this).attr("data", "imadesyo");
+			}
 		});
 
 		$("button.like_link span").each(function(){
-			var txt4 = $(this).html();
-			$(this).html(
-				txt4.replace(/いいね/g, "今でしょ")
-			);
-			$(this).attr("title", "今でしょ")
+			if($(this).attr("data") != "imadesyo"){
+				var txt4 = $(this).html();
+				$(this).html(
+					txt4.replace(/いいね/g, "今でしょ")
+				);
+				$(this).attr("title", "今でしょ")
+				$(this).attr("data", "imadesyo");
+			}
 		});
 
 		$("a.uiIconText").each(function(){
-			var txt5 = $(this).html();
-			$(this).html(
-				txt5.replace(/いいね/g, "今でしょ")
-			);
+			if($(this).attr("data") != "imadesyo"){
+				var txt5 = $(this).html();
+				$(this).html(
+					txt5.replace(/いいね/g, "今でしょ")
+				);
+				$(this).attr("data", "imadesyo");
+			}
 		});
 
 		$("span.uiButtonText").each(function(){
-			var txt6 = $(this).html();
-			$(this).html(
-				txt6.replace(/いいね/g, "今でしょ")
-			);
+			if($(this).attr("data") != "imadesyo"){
+				var txt6 = $(this).html();
+				$(this).html(
+					txt6.replace(/いいね/g, "今でしょ")
+				);
+				$(this).attr("data", "imadesyo");
+			}
 		});
 
 		$("div.uiStreamAttachments").each(function(){
